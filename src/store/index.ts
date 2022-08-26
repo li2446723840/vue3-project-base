753a047e5a8aab4daf53607db90c1ff14e5fb6d6
li2446723840 <WSL19951995710jianli>
2022-08-26 15:59:54 +0800

    添加注释

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { createPinia } from "pinia";
-import piniaPluginPersist from "pinia-plugin-persist";
+import piniaPluginPersist from "pinia-plugin-persist"; // 数据持久化插件
 
 const store = createPinia();
 store.use(piniaPluginPersist);
